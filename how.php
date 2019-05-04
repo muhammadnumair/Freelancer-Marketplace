839486629acc82bf845abe3e7741682d8113b675
Muhammad Numair <numair.cs@gmail.com>
2019-05-04 22:40:24 +0500

    How Page Update

---
+++ how.php
@@ -0,0 +1,147 @@
+<?php include_once('extras/database.php'); ?>
+<?php session_start(); ?>
+<?php include_once('extras/functions.php'); ?>
+<?php include_once('layouts/header.php'); ?>
+<!-- ==============================================
+   Header Section
+   =============================================== -->	
+<section class="how-banner">
+   <div class="container">
+      <h4>How it works</h4>
+   </div>
+   <!-- /.container -->
+</section>
+<!-- ==============================================
+   How Section
+   =============================================== -->	
+<section class="how">
+   <div class="container">
+      <div class="col-lg-3 text-center">
+         <i class="fa fa-file fa-5x"></i>
+         <h4>Post projects to tell us what you need done.</h4>
+      </div>
+      <div class="col-lg-3 text-center">
+         <i class="fa fa-users fa-5x"></i>
+         <h4>Browse profiles, reviews, then hire your most favorite and start project.</h4>
+      </div>
+      <div class="col-lg-3 text-center">
+         <i class="fa fa-briefcase fa-5x"></i>
+         <h4>Use The Kafe platform to chat and share files</h4>
+      </div>
+      <div class="col-lg-3 text-center">
+         <i class="fa fa-usd fa-5x"></i>
+         <h4>With our protection, money is only paid for work you authorize.</h4>
+      </div>
+   </div>
+   <!-- /.container -->
+</section>
+<!-- ==============================================
+   Category Section
+   =============================================== -->
+<div class="tr-category section-padding">
+   <div class="container">
+      <div class="section-title">
+         <h1>Browse Freelancers By Category</h1>
+      </div>
+      <div class="row">
+         <div class="category-list tr-list">
+            <div class="col-lg-3">
+               <div class="category-box">
+                  <a href="hire.html">
+                  <span class="icon"><i class="fa fa-code fa-2x"></i></span>
+                  <span class="category-title">Web & Mobile Development</span>
+                  <span class="category-quantity">(1298)</span>
+                  </a>
+               </div>
+               <!-- category-box -->
+            </div>
+            <!-- col-lg-3 -->
+            <div class="col-lg-3">
+               <div class="category-box">
+                  <a href="hire.html">
+                  <span class="icon"><i class="fa fa-eye fa-2x"></i></span>
+                  <span class="category-title">Design, Arts & Multimedia</span>
+                  <span class="category-quantity">(76212)</span>
+                  </a>
+               </div>
+               <!-- category-box -->
+            </div>
+            <!-- col-lg-3 -->
+            <div class="col-lg-3">
+               <div class="category-box">
+                  <a href="hire.html">
+                  <span class="icon"><i class="fa fa-edit fa-2x"></i></span>
+                  <span class="category-title">Writing & Translation</span>
+                  <span class="category-quantity">(212)</span>
+                  </a>
+               </div>
+               <!-- category-box -->
+            </div>
+            <!-- col-lg-3 -->
+            <div class="col-lg-3">
+               <div class="category-box">
+                  <a href="hire.html">
+                  <span class="icon"><i class="fa fa-edit fa-2x"></i></span>
+                  <span class="category-title">Admin Support</span>
+                  <span class="category-quantity">(972)</span>
+                  </a>
+               </div>
+               <!-- category-box -->
+            </div>
+            <!-- col-lg-3 -->
+         </div>
+         <!-- category-list -->
+         <div class="category-list tr-list">
+            <div class="col-lg-3">
+               <div class="category-box">
+                  <a href="hire.html">
+                  <span class="icon"><i class="fa fa-table fa-2x"></i></span>
+                  <span class="category-title">Management & Finance</span>
+                  <span class="category-quantity">(1298)</span>
+                  </a>
+               </div>
+               <!-- category-box -->
+            </div>
+            <!-- col-lg-3 -->
+            <div class="col-lg-3">
+               <div class="category-box">
+                  <a href="hire.html">
+                  <span class="icon"><i class="fa fa-bullhorn fa-2x"></i></span>
+                  <span class="category-title">Sales & Marketing</span>
+                  <span class="category-quantity">(76212)</span>
+                  </a>
+               </div>
+               <!-- category-box -->
+            </div>
+            <!-- col-lg-3 -->
+            <div class="col-lg-3">
+               <div class="category-box">
+                  <a href="hire.html">
+                  <span class="icon"><i class="fa fa-wrench fa-2x"></i></span>
+                  <span class="category-title">Engineering & Architecture</span>
+                  <span class="category-quantity">(1298)</span>
+                  </a>
+               </div>
+               <!-- category-box -->
+            </div>
+            <!-- col-lg-3 -->
+            <div class="col-lg-3">
+               <div class="category-box">
+                  <a href="hire.html">
+                  <span class="icon"><i class="fa fa-legal fa-2x"></i></span>
+                  <span class="category-title">Legal</span>
+                  <span class="category-quantity">(76212)</span>
+                  </a>
+               </div>
+               <!-- category-box -->
+            </div>
+            <!-- col-lg-3 -->
+         </div>
+         <!-- category-list -->
+      </div>
+      <!-- row -->
+   </div>
+   <!-- /.container -->
+</div>
+<!-- tr-category -->	
+<?php include_once('layouts/footer.php'); ?>
